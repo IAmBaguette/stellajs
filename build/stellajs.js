2921acb8589b786a9bfcc05da668e44b7719732c
IAmBaguette <IAmBaguette@users.noreply.github.com>
2016-06-03 23:52:05 -0400

    Changed State functionality to States

--- build/stellajs.js
+++ build/stellajs.js
@@ -18,39 +18,62 @@ var app = (function (canvas) {
     var ctx = canvas.getContext("2d");
     this.fps = 60;
     this.state = undefined;
+    this.states = {};
+
+    this.add = function (key, state) {
+        if (!this.states.hasOwnProperty(key)) {
+            this.states.key = state;
+        }
+    };
+
+    this.remove = function (key) {
+        if (this.states.hasOwnProperty(key)) {
+            delete this.states.key;
+        }
+    }
+
+    this.set = function (key) {
+        this.state = new this.states.key(self);
+    };
 
     this.input = new input();
 
     this.start = function () {
-        if (this.state) {
-            canvas.addEventListener("keydown", function (e) {
-                if (!self.input.keys[e.keyCode]) {
-                    self.input.keyPress[e.keyCode] = true;
-                }
-                self.input.keys[e.keyCode] = true;
+        window.onresize = function () {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
 
-                if (DEBUG_MODE) {
-                    if (self.input.getKeyDown(e.keyCode)) {
-                        var log = "";
-                        for (var attr in Keyboard) {
-                            if (Keyboard[attr] == e.keyCode) {
-                                log += "Keyboard." + attr + ": ";
-                            }
+            self.draw();
+        };
+
+        window.onresize();
+        canvas.focus();
+
+        canvas.addEventListener("keydown", function (e) {
+            if (!self.input.keys[e.keyCode]) {
+                self.input.keyPress[e.keyCode] = true;
+            }
+            self.input.keys[e.keyCode] = true;
+
+            if (DEBUG_MODE) {
+                if (self.input.getKeyPress(e.keyCode)) {
+                    var log = "";
+                    for (var attr in Keyboard) {
+                        if (Keyboard[attr] == e.keyCode) {
+                            log += "Keyboard." + attr + ": ";
                         }
-                        log += e.keyCode;
-                        console.log(log);
                     }
+                    log += e.keyCode;
+                    console.log(log);
                 }
-            });
+            }
+        });
 
-            canvas.addEventListener("keyup", function (e) {
-                self.input.keys[e.keyCode] = false;
-            });
+        canvas.addEventListener("keyup", function (e) {
+            self.input.keys[e.keyCode] = false;
+        });
 
-            loop();
-        } else {
-            console.error("state is undefined");
-        }
+        loop();
     };
 
     var loop = function () {
