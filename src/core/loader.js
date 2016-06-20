cf569bad974bf60a635f9cb3be185b4c3af8afa4
IAmBaguette <IAmBaguette@users.noreply.github.com>
2016-06-20 18:46:50 -0400

    simply Loader class
    Added the ability to add assets using an array
    Added the ability to get assets using an array

--- src/core/loader.js
+++ src/core/loader.js
@@ -25,15 +25,49 @@ var loader = (function (callback) {
         }
     };
 
-    this.add = function (key, img) {
+    this.add = function () {
+        if (arguments.length == 2) {
+            this.add.twoParams.apply(this, arguments);
+        } else if (arguments.length == 1) {
+            this.add.oneParam.apply(this, arguments);
+        }
+    };
+
+    // { "key": "src" }
+    this.add.oneParam = function (obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                this.add(key, obj[key]);
+            }
+        }
+    };
+
+    this.add.twoParams = function (key, src) {
         if (!this.loaded.hasOwnProperty(key)) {
-            this.loaded[key] = img;
+            this.loaded[key] = src;
         } else {
             console.error("can't the same key twice");
         }
     };
-    
-    this.get = function (key) {
+
+    this.get = function (obj) {
+        if (obj instanceof Array) {
+            return this.get.array.apply(this, arguments);
+        } else {
+            return this.get.key.apply(this, arguments);
+        }
+    };
+
+    // [key]
+    this.get.array = function (array) {
+        var results = [];
+        array.forEach(function (key) {
+            results.push(this.get(key));
+        }, this);
+        return results;
+    };
+
+    this.get.key = function (key) {
         if (this.loaded.hasOwnProperty(key)) {
             return this.loaded[key];
         }
