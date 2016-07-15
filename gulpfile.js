6d1fa8328584f31c7f0614e614cbbbf3b6daa62e
IAmBaguette <IAmBaguette@users.noreply.github.com>
2016-07-15 16:30:57 -0400

    Initial re-work of stella and Cameras

--- gulpfile.js
+++ gulpfile.js
@@ -14,19 +14,21 @@ gulp.task("watch", function () {
 gulp.task("build", function () {
     gulp.src(
         [
-            "./src/core/vector.js",
-            "./src/core/utils.js",
-            "./src/core/grid.js",
-            "./src/core/camera.js",
-            "./src/core/keycode.js",
-            "./src/core/input.js",
-            "./src/core/loader.js",
-            "./src/core/anim.js",
-            "./src/core/animates.js",
-            "./src/core/state.js",
-            "./src/core/stellajs.js"
+            // "./src/core/vector.js",
+            // "./src/core/utils.js",
+            // "./src/core/grid.js",
+            // "./src/core/camera.js",
+            // "./src/core/keycode.js",
+            // "./src/core/input.js",
+            // "./src/core/loader.js",
+            // "./src/core/anim.js",
+            // "./src/core/animates.js",
+            // "./src/core/state.js",
+            // "./src/core/stellajs.js"
+            "./src/stella.js"
         ])
-        .pipe(concat("stellajs.js"))
+        // .pipe(concat("stellajs.js"))
+        .pipe(concat("stella.js"))
         //.pipe(stripDebug())
         .pipe(strip())
         //.pipe(header("//TEST\n"))
